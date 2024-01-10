592a850b1fb5adb3c91a54fe38e43899d24b4517
YoshiGuy2000 <danielavilalearns@gmail.com>
2024-01-10 12:31:13 -0500

    Created initial etch a sketch layout

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("gridContainer")
+let rows = documnet.getElementById("gridRow")
+
+function makeGrid(a) {
+    for (i = 0; i < (a * a); i++) {
+        let row = document.createElement("div")
+        container.appendChild(row).className = "gridRow"
+    }
+    document.documentElement.style.setProperty("--rowNum", a)
+    document.documentElement.style.setProperty("--colNum", a)
+}
+
+makeGrid(10)
